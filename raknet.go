d85c36b2651c5a4d2966a53b2c83a4833c614964
beito123 <beito123@gmx.com>
2018-04-06 15:28:22 +0900

    Add raknet.go

---
+++ raknet.go
@@ -0,0 +1,20 @@
+package raknet
+
+/*
+ * go-raknet
+ *
+ * Copyright (c) 2018 beito
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+const (
+
+	// Version is version of go-raknet library
+	Version = "1.0.0"
+
+	// ProtocolVersion is version of Raknet protocol
+	ProtocolVersion = 8
+)
+
