bef107d7a83b2d41489925a3661b48a70bc6eebd
beito123 <beito123@gmx.com>
2018-04-08 00:02:43 +0900

    Add Magic functions in RaknetStream

--- binary/stream.go
+++ binary/stream.go
@@ -23,53 +23,63 @@ type RaknetStream struct {
 	binary.Stream
 }
 
+// IsMagic returns whether 16bytes is Raknet magic
+func (rs *RaknetStream) IsMagic() bool {
+	return string(rs.Get(len(raknet.Magic))) == string(raknet.Magic) // bad hack? :P// but fast..
+}
+
+// PutMagic write Raknet Magic
+func (rs *RaknetStream) PutMagic() error {
+	return rs.Put(raknet.Magic)
+}
+
 // Triad sets triad got from buffer to value
-func (bs *RaknetStream) Triad(value *Triad) error {
-	return Read(bs.Buffer, BigEndian, value)
+func (rs *RaknetStream) Triad(value *Triad) error {
+	return Read(rs.Buffer, BigEndian, value)
 }
 
 // PutTriad puts triad from value to buffer
-func (bs *RaknetStream) PutTriad(value Triad) error {
-	return Write(bs.Buffer, BigEndian, value)
+func (rs *RaknetStream) PutTriad(value Triad) error {
+	return Write(rs.Buffer, BigEndian, value)
 }
 
 // LTriad sets triad got from buffer as LittleEndian to value
-func (bs *RaknetStream) LTriad(value *Triad) error {
-	return Read(bs.Buffer, LittleEndian, value)
+func (rs *RaknetStream) LTriad(value *Triad) error {
+	return Read(rs.Buffer, LittleEndian, value)
 }
 
 // PutLTriad puts triad from value to buffer as LittleEndian
-func (bs *RaknetStream) PutLTriad(value Triad) error {
-	return Write(bs.Buffer, LittleEndian, value)
+func (rs *RaknetStream) PutLTriad(value Triad) error {
+	return Write(rs.Buffer, LittleEndian, value)
 }
 
 // String sets string(len short, str string) got from buffer to value
-func (bs *RaknetStream) String(value *string) error {
+func (rs *RaknetStream) String(value *string) error {
 	var n uint16
-	err := bs.Short(&n)
+	err := rs.Short(&n)
 	if err != nil {
 		return err
 	}
 
-	*value = string(bs.Get(int(n)))
+	*value = string(rs.Get(int(n)))
 	return nil
 }
 
 // PutString puts string(len short, str string) to Buffer
-func (bs *RaknetStream) PutString(value string) error {
+func (rs *RaknetStream) PutString(value string) error {
 	n := uint16(len(value))
-	err := bs.PutShort(n)
+	err := rs.PutShort(n)
 	if err != nil {
 		return err
 	}
-	return bs.Put([]byte(value))
+	return rs.Put([]byte(value))
 }
 
 // Address sets address got from Buffer to addr and port
 // address(version byte, address byte x4, port ushort)
-func (bs *RaknetStream) Address(addr *string, port *uint16) error {
+func (rs *RaknetStream) Address(addr *string, port *uint16) error {
 	var version byte
-	err := bs.Byte(&version)
+	err := rs.Byte(&version)
 	if err != nil {
 		return err
 	}
@@ -79,7 +89,7 @@ func (bs *RaknetStream) Address(addr *string, port *uint16) error {
 	if version == 4 {
 		var bytes byte
 		for i := 0; i < 4; i++ {
-			err = bs.Byte(&bytes)
+			err = rs.Byte(&bytes)
 			if err != nil {
 				return err
 			}
@@ -91,7 +101,7 @@ func (bs *RaknetStream) Address(addr *string, port *uint16) error {
 		}
 		addr = &address
 
-		err = bs.Short(port)
+		err = rs.Short(port)
 		if err != nil {
 			return err
 		}
@@ -104,8 +114,8 @@ func (bs *RaknetStream) Address(addr *string, port *uint16) error {
 
 // PutAddress puts address to Buffer
 // address(version byte, address byte x4, port ushort)
-func (bs *RaknetStream) PutAddress(addr string, port uint16, version byte) error {
-	err := bs.PutByte(version)
+func (rs *RaknetStream) PutAddress(addr string, port uint16, version byte) error {
+	err := rs.PutByte(version)
 	if err != nil {
 		return err
 	}
@@ -113,12 +123,12 @@ func (bs *RaknetStream) PutAddress(addr string, port uint16, version byte) error
 	if version == 4 {
 		for _, str := range strings.Split(addr, ".") {
 			i, _ := strconv.Atoi(str)
-			err = bs.PutByte(^byte(i) & 0xff)
+			err = rs.PutByte(^byte(i) & 0xff)
 			if err != nil {
 				return err
 			}
 		}
-		err = bs.PutShort(port)
+		err = rs.PutShort(port)
 		if err != nil {
 			return err
 		}
@@ -130,11 +140,11 @@ func (bs *RaknetStream) PutAddress(addr string, port uint16, version byte) error
 }
 
 // AddressSystemAddress sets address got from Buffer to SystemAddress
-func (bs *RaknetStream) AddressSystemAddress(addr *raknet.SystemAddress) error {
+func (rs *RaknetStream) AddressSystemAddress(addr *raknet.SystemAddress) error {
 	var add string
 	var port uint16
 
-	err := bs.Address(&add, &port)
+	err := rs.Address(&add, &port)
 	if err != nil {
 		return err
 	}
@@ -147,13 +157,13 @@ func (bs *RaknetStream) AddressSystemAddress(addr *raknet.SystemAddress) error {
 }
 
 // PutAddressSystemAddress puts address from UDPAddr to Buffer
-func (bs *RaknetStream) PutAddressSystemAddress(addr raknet.SystemAddress) error {
-	return bs.PutAddress(addr.IP.String(), addr.Port, byte(addr.Version()))
+func (rs *RaknetStream) PutAddressSystemAddress(addr raknet.SystemAddress) error {
+	return rs.PutAddress(addr.IP.String(), addr.Port, byte(addr.Version()))
 }
 
 // UUID reads UUID
-func (bs *RaknetStream) UUID(uid *uuid.UUID) error {
-	u, err := uuid.FromBytes(bs.Get(16))
+func (rs *RaknetStream) UUID(uid *uuid.UUID) error {
+	u, err := uuid.FromBytes(rs.Get(16))
 	if err != nil {
 		return err
 	}
@@ -164,36 +174,36 @@ func (bs *RaknetStream) UUID(uid *uuid.UUID) error {
 }
 
 // PutUUID writes UUID
-func (bs *RaknetStream) PutUUID(uid uuid.UUID) error {
-	return bs.Put(uid.Bytes())
+func (rs *RaknetStream) PutUUID(uid uuid.UUID) error {
+	return rs.Put(uid.Bytes())
 }
 
 // ConnectionType reads ConnectionType
-func (bs *RaknetStream) ConnectionType(typ *raknet.ConnectionType) error {
+func (rs *RaknetStream) ConnectionType(typ *raknet.ConnectionType) error {
 	var ntyp raknet.ConnectionType
 
-	err := bs.UUID(&ntyp.UUID)
+	err := rs.UUID(&ntyp.UUID)
 	if err != nil {
 		return err
 	}
 
-	err = bs.String(&ntyp.Name)
+	err = rs.String(&ntyp.Name)
 	if err != nil {
 		return err
 	}
 
-	err = bs.String(&ntyp.Lang)
+	err = rs.String(&ntyp.Lang)
 	if err != nil {
 		return err
 	}
 
-	err = bs.String(&ntyp.Version)
+	err = rs.String(&ntyp.Version)
 	if err != nil {
 		return err
 	}
 
 	var metaLen byte
-	err = bs.Byte(&metaLen)
+	err = rs.Byte(&metaLen)
 	if err != nil {
 		return err
 	}
@@ -202,12 +212,12 @@ func (bs *RaknetStream) ConnectionType(typ *raknet.ConnectionType) error {
 
 	var key, value string
 	for i := byte(0); i < metaLen; i++ {
-		err = bs.String(&key)
+		err = rs.String(&key)
 		if err != nil {
 			return err
 		}
 
-		err = bs.String(&value)
+		err = rs.String(&value)
 		if err != nil {
 			return err
 		}
@@ -224,23 +234,23 @@ func (bs *RaknetStream) ConnectionType(typ *raknet.ConnectionType) error {
 }
 
 // PutConnectionType writes ConnectionType
-func (bs *RaknetStream) PutConnectionType(typ raknet.ConnectionType) error {
-	err := bs.PutUUID(typ.UUID)
+func (rs *RaknetStream) PutConnectionType(typ raknet.ConnectionType) error {
+	err := rs.PutUUID(typ.UUID)
 	if err != nil {
 		return err
 	}
 
-	err = bs.PutString(typ.Name)
+	err = rs.PutString(typ.Name)
 	if err != nil {
 		return err
 	}
 
-	err = bs.PutString(typ.Lang)
+	err = rs.PutString(typ.Lang)
 	if err != nil {
 		return err
 	}
 
-	err = bs.PutString(typ.Version)
+	err = rs.PutString(typ.Version)
 	if err != nil {
 		return err
 	}
@@ -249,18 +259,18 @@ func (bs *RaknetStream) PutConnectionType(typ raknet.ConnectionType) error {
 		return errors.New("too many metadata values")
 	}
 
-	err = bs.PutByte(byte(len(typ.Metadata)))
+	err = rs.PutByte(byte(len(typ.Metadata)))
 	if err != nil {
 		return err
 	}
 
 	for k, v := range typ.Metadata {
-		err = bs.PutString(k)
+		err = rs.PutString(k)
 		if err != nil {
 			return err
 		}
 
-		err = bs.PutString(v)
+		err = rs.PutString(v)
 		if err != nil {
 			return err
 		}
