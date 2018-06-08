c50b55fa7a4590516343fc728f8d63202daf4a78
beito123 <beito123@users.noreply.github.com>
2018-06-08 22:26:51 +0900

    Add HandleSendPacket in Handler

--- server/handler.go
+++ server/handler.go
@@ -39,6 +39,9 @@ type Handler interface {
 	// CloseConn is called on a session is closed
 	CloseConn(uid int64)
 
+	// HandleSendPacket handles a packet sent from the server to a client
+	HandleSendPacket(addr net.Addr, pk raknet.Packet)
+
 	// HandleRawPacket handles a raw packet no processed in Raknet server
 	HandleRawPacket(addr net.Addr, pk raknet.Packet)
 
@@ -48,45 +51,3 @@ type Handler interface {
 	// HandleUnknownPacket handles a unknown packet
 	HandleUnknownPacket(uid int64, pk raknet.Packet)
 }
-
-/*
-// StartServer is called on the server is started
-func (Handler) StartServer() {
-}
-
-// CloseServer is called on the server is closed
-func (Handler) CloseServer() {
-}
-
-// HandlePing is called on a ping packet is received
-func (Handler) HandlePing(addr *net.Addr) {
-}
-
-// OpenPreConn is called before a new session is created
-func (Handler) OpenPreConn(addr *net.Addr) {
-}
-
-// OpenConn is called on a new session is created
-func (Handler) OpenConn(uid int64, addr *net.Addr) {
-}
-
-// ClosePreConn is called before a session is closed
-func (Handler) ClosePreConn(uid int64) {
-}
-
-// CloseConn is called on a session is closed
-func (Handler) CloseConn(uid int64) {
-}
-
-// HandleRawPacket handles a raw packet no processed in Raknet server
-func (Handler) HandleRawPacket(uid int64, pk raknet.Packet) {
-}
-
-// HandlePacket handles a message packet
-func (Handler) HandlePacket(uid int64, pk raknet.Packet) {
-}
-
-// HandleUnknownPacket handles a unknown packet
-func (Handler) HandleUnknownPacket(uid int64, pk raknet.Packet) {
-}
-*/
